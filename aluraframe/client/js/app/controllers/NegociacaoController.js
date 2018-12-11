f49d38c31d3fc2d1e25b1211a13f5a9a9ac78e8b
natizui <izui.nm@gmail.com>
2018-12-11 13:06:47 -0200

    Aula 03 - atividade 05

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,8 +11,9 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        console.log(typeof(this._inputData.value));
-        console.log(this._inputData.value);
+        let data = new Date(this._inputData.value.replace(/-/g, '-'));
+
+        console.log(data);
 
         // let negociacao = new Negociacao(
         //     this._inputData.value,
