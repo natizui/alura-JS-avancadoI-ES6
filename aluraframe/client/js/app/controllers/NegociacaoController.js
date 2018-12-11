d17d9f1ee7290fb56b05dca9d0376d2d7ebc510b
natizui <izui.nm@gmail.com>
2018-12-11 12:50:51 -0200

    Aula 03 - atividade 03

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,19 +1,18 @@
 class NegociacaoController {
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document); //bind é usado para que o $ mantenha a associação com o document
+        this.inputData = $("#data");
+        this.inputQuantidade = $("#quantidade");
+        this.inputValor = $("#valor");
+    }
+
     adiciona(event) {
         event.preventDefault();
 
-        // let inputData = document.querySelector("#data");
-        // let inputQuantidade = document.querySelector("#quantidade");
-        // let inputValor = document.querySelector("#valor");
-        //para escrever menos:
-        let $ = document.querySelector.bind(document); //bind é usado para que o $ mantenha a associação com o document
-        let inputData = $("#data");
-        let inputQuantidade = $("#quantidade");
-        let inputValor = $("#valor");
-
-        console.log(inputData);
-        console.log(inputQuantidade);
-        console.log(inputValor);
+        console.log(this.inputData);
+        console.log(this.inputQuantidade);
+        console.log(this.inputValor);
     }
 }
