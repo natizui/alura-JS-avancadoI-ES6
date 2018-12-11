8b81a9cf88d8de60bdcc0e2a659e9bac9ad6d6a3
natizui <izui.nm@gmail.com>
2018-12-11 13:23:34 -0200

    Aula 03 - atividade 09

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -14,23 +14,18 @@ class NegociacaoController {
         let data = new Date(...  //... é um spread operator, indica que o array será desmembrado - e o primeiro item do array, e cada parâmetro do Date será posicionado na mesma ordem no construtor.
             this._inputData.value
                 .split('-')
-                .map((item, indice) => {
-                    // if(indice == 1) return item - 1;
-                    // return item;
-                    //pra ficar mais simples:
-                    return item - indice % 2;
-                })
+                .map((item, indice) => item - indice % 2)
         ); 
 
         console.log(data);
 
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
 
-        // console.log(negociacao);
+        console.log(negociacao);
     }
 
     
